df6901eaa0a25c318f5fdc35362c17008d39c4c5
Jonathan LE QUERE <jle-quer@e3r10p14.42.fr>
2017-09-12 13:00:57 +0200

    First Commit: Add Folders and Files

---
+++ Colleen/Colleen.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * ONE COMMENT
+ */
+void function(){}
+int main(){char*a="#include<stdio.h>%c/*%c * ONE COMMENT%c */%cvoid function(){}%cint main(){char*a=%c%s%c%c/*%c * ONE MORE COMMENT%c */%c;printf(a,10,10,10,10,10,34,a,34,10,10,10,10,10);}%c"
+/*
+ * ONE MORE COMMENT
+ */
+;printf(a,10,10,10,10,10,34,a,34,10,10,10,10,10);}
