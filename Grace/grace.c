dff91ad905abaf6ab9dcd6b04f8bc7f8282620d8
Jonathan LE QUERE <jle-quer@e3r10p14.42.fr>
2017-09-12 13:09:57 +0200

    Changes name

--- Grace/grace.c
+++ Grace/grace.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define inutile lol
-#define yenfaut3 haha
+#define FIRSTDEFINE 0
+#define SECONDDEFINE 1
 /*
  * Comment
  */
-#define FT(x) int main(){FILE *txt;txt = fopen("Grace_kid.c", "w+");char *a="#include <stdio.h>%c#include <stdlib.h>%c#define inutile lol%c#define yenfaut3 haha%c/*%c * Comment%c */%c#define FT(x) int main(){FILE *txt;txt = fopen(%cGrace_kid.c%c, %cw+%c);char *a=%c%s%c;fprintf(txt,a, 10, 10, 10, 10, 10, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);}%cFT(x);%c";fprintf(txt,a, 10, 10, 10, 10, 10, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);}
+#define FT(x) int main(){FILE *txt;txt = fopen("Grace_kid.c", "w+");char *a="#include <stdio.h>%c#include <stdlib.h>%c#define FIRSTDEFINE 0%c#define SECONDDEFINE 1%c/*%c * Comment%c */%c#define FT(x) int main(){FILE *txt;txt = fopen(%cGrace_kid.c%c, %cw+%c);char *a=%c%s%c;fprintf(txt,a, 10, 10, 10, 10, 10, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);}%cFT(x);%c";fprintf(txt,a, 10, 10, 10, 10, 10, 10, 10, 34, 34, 34, 34, 34, a, 34, 10, 10);}
 FT(x);
