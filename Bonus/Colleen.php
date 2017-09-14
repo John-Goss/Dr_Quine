9f8b1b894fbe7f0df6360548d464b1a6c4f7cd6a
Jonathan LE QUERE <jle-quer@e3r10p14.42.fr>
2017-09-14 17:04:09 +0200

    Final push

--- Bonus/Colleen.php
+++ Bonus/Colleen.php
@@ -1,20 +1,20 @@
 <?php
-//
-//	ONE COMMENT
-//
+/*
+	ONE COMMENT
+*/
 function foo()
 {
-	//
-	//	SECOND COMMENT
-	//
-	$str = "<?php%c//%c//%cONE COMMENT%c//%cfunction foo()%c{%c%c//%c%c//%cSECOND COMMENT%c%c//%c%c%cstr = %c%s%c;%c%c%cing = sprintf(%cstr,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,36,34,%cstr,34,10,9,36,36,36,10,9,36,10,10,10,10,9,10,10,10,10);%c%cecho %cing;%c}%cfunction maine()%c{%c%cfoo();%c}%cmaine();%c?>%c";
-	$ing = sprintf($str,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,36,34,$str,34,10,9,36,36,36,10,9,36,10,10,10,10,9,10,10,10,10);
-	echo $ing;
+	$str = "<?php%c/*%c%cONE COMMENT%c*/%cfunction foo()%c{%c%c%cstr = %c%s%c;%c%c%cout = sprintf(%cstr,10,10,9,10,10,10,10,9,36,34,%cstr,34,10,9,36,36,36,10,9,36,10,10,10,10,10,9,10,10,9,10,10,10,10);%c%cecho %cout;%c}%cfunction main()%c{%c/*%c%cSECOND COMMENT%c*/%c%cfoo();%c}%cmain();%c?>%c";
+	$out = sprintf($str,10,10,9,10,10,10,10,9,36,34,$str,34,10,9,36,36,36,10,9,36,10,10,10,10,10,9,10,10,9,10,10,10,10);
+	echo $out;
 }
-function maine()
+function main()
 {
+/*
+	SECOND COMMENT
+*/
 	foo();
 }
-maine();
+main();
 ?>
 
